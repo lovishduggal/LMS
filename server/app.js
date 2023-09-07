45971991e685b0f7989366cdb67bd3c053324712
Lovish Duggal <lovishduggal11@gmail.com>
2023-09-07 14:42:23 +0530

    setup the project and a basic server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+    })
+);
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use('/ping', function (req, res) {
+    return res.send('<h1>PONG</h1>');
+});
+
+app.all('*', function (req, res) {
+    return res.status(404).send('OOPS!! 404 PAGE NOT FOUND');
+});
+
+export default app;
