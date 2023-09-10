235936c722adf8e88382bbe0bb2cdcfa35d73fe3
Lovish Duggal <lovishduggal11@gmail.com>
2023-09-10 15:44:22 +0530

    Defined AppError class and errorMiddleware to handle the errors at the application level

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,8 @@
-const handleRegister = (req, res) => {
+import AppError from '../utils/error.util';
+const handleRegister = (req, res, next) => {
     const { fullName, email, password } = req.body;
     if (!fullName || !email || !password) {
+        return next(new AppError('All fields are required', 400));
         //! Tomorrow, I will begin from this point.
     }
 };
