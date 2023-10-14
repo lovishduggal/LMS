5111a46fc99a8214fe44bfdd7101d315f49d3bc6
Lovish Duggal <lovishduggal11@gmail.com>
2023-10-14 10:22:50 +0530

    Setting up redux store and basic auth slice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,4 @@
-import toast, { Toaster } from 'react-hot-toast';
-
-const notify = () => toast('Here is your toast.');
-
 const App = () => {
-    return (
-        <div>
-            <button onClick={notify}>Make me a toast</button>
-            <Toaster />
-        </div>
-    );
+    return;
 };
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import store from './Redux/store.js';
 import App from './App.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider value={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
