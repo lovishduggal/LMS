8ece10a166b71ce8f508ca363988222b479fcf1f
Lovish Duggal <lovishduggal11@gmail.com>
2023-10-17 15:10:16 +0530

    done the HomePage and added login signup btns to the sidebar drawer

--- frontend/src/Layouts/HomeLayout.jsx
+++ frontend/src/Layouts/HomeLayout.jsx
@@ -1,7 +1,14 @@
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { FiMenu } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { FiLogOut, FiMenu } from 'react-icons/fi';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 function HomeLayout({ children }) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+    const role = useSelector((state) => state?.auth?.role);
+
     function fiMenu(size) {
         const fiMenu = document.querySelector('.drawer-content label > svg');
         fiMenu.style.width = fiMenu.style.height = `${size}px`;
@@ -19,6 +26,13 @@ function HomeLayout({ children }) {
         fiMenu(32);
         changeWidth(0);
     }
+
+    function handleLogout(e) {
+        e.preventDefault();
+        //dispatch logout action.
+        // const res = await dispatch(logout());
+        //if(res?.payload?.success) naviagte('/);
+    }
     return (
         <div className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-fit">
@@ -34,7 +48,7 @@ function HomeLayout({ children }) {
                         <FiMenu
                             onClick={() => changeWidth('fit-content')}
                             size={'32px'}
-                            className="font-bold  absolute left-4 top-4 text-white"
+                            className="font-bold m-4 text-white"
                         />
                     </label>
                 </div>
@@ -43,7 +57,7 @@ function HomeLayout({ children }) {
                         htmlFor="my-drawer"
                         aria-label="close sidebar"
                         className="drawer-overlay"></label>
-                    <ul className="menu p-4  w-48 sm:w-80 relative bg text-base">
+                    <ul className="menu p-4  w-48 sm:w-80 relative bg text-base space-y-4 h-[100vh]">
                         <li className="w-fit absolute right-2 z-50">
                             <button>
                                 <AiFillCloseCircle
@@ -55,6 +69,13 @@ function HomeLayout({ children }) {
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard">
+                                    Admin Dashboard
+                                </Link>
+                            </li>
+                        )}
                         <li>
                             <Link to="/courses">All courses</Link>
                         </li>
@@ -64,6 +85,33 @@ function HomeLayout({ children }) {
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+                        {!isLoggedIn && (
+                            <li className="absolute bottom-[6rem] w-[90%] mx-auto">
+                                <div className="w-full flex items-end justify-center">
+                                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/login">Login</Link>
+                                    </button>
+                                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/signup">Signup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+
+                        {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%] mx-auto">
+                                <div className="w-full flex items-end justify-center">
+                                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link onClick={handleLogout}>
+                                            Logout
+                                        </Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,8 +1,47 @@
-import HomeLayout from '../Layouts/HomeLayout';
+import { Link } from 'react-router-dom';
 
+import HPM from '../Assets/images/HPM.png';
+import HomeLayout from '../Layouts/HomeLayout';
 function HomePage() {
-    return <HomeLayout>
-        <div></div>
-    </HomeLayout>;
+    return (
+        <HomeLayout>
+            <div className="pt-16 text-white flex  flex-col items-center justify-center md:flex-row  gap-10 h-[90vh] w-[90%] m-auto text-center md:text-start">
+                <div className="w-full h-full md:w-1/2 space-y-6 flex justify-center items-center">
+                    <div className="space-y-6 ">
+                        <h1 className="text-2xl  sm:text-3xl lg:text-5xl font-semibold">
+                            Find out Best{' '}
+                            <span className="text-yellow-500 font-bold">
+                                Online Courses
+                            </span>
+                        </h1>
+                        <p className="text-sm  sm:text-[1rem] lg:text-xl text-gray-200">
+                            We have a large library of courses taught by highly
+                            skilled and qualified faculties at a very affordable
+                            cost.
+                        </p>
+                        <div className="flex flex-col items-center justify-center sm:flex-row md:justify-start gap-4">
+                            <Link to="/courses">
+                                <button className="text-sm bg-yellow-500 px-5 py-3 rounded-md font-semibold  cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300 text-white">
+                                    Explore courses
+                                </button>
+                            </Link>
+                            <Link to="/contact">
+                                <button className="text-sm border border-yellow-500  px-5 py-3 rounded-md font-semibold cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300 text-white">
+                                    Contact Us
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full  h-full md:w-1/2 md:h-auto flex justify-center items-center">
+                    <img
+                        className="object-cover"
+                        src={HPM}
+                        alt="homepagee image"
+                    />
+                </div>
+            </div>
+        </HomeLayout>
+    );
 }
 export default HomePage;
